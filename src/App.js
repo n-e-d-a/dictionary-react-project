b2a8d27264856c54b985e03c003ddd3753b22737
n-e-d-a <87589027+n-e-d-a@users.noreply.github.com>
2021-11-04 12:15:45 +0330

    add keyword usestate

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-       
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          Coded by{" "}
+          <a href="https://xenodochial-poitras-5501a5.netlify.app">
+            {" "}
+            Neda Zare
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-
---
+++ src/Dictionary.js
@@ -0,0 +1,22 @@
+import {React,useState} from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+let [keyword,setKeyword]=useState(null);
+
+    function search(event){
+        event.preventDefault();
+        alert(`Searching for the ${keyword} definition`);
+    }
+function handleKeywordChange(event){
+    setKeyword(event.target.value);
+}
+
+  return (
+  <div className="dictionary">
+      <form onSubmit={search}>
+          <input type="search" autoFocus={true} onChange={handleKeywordChange}/>
+      </form>
+  </div>
+  );
+}
