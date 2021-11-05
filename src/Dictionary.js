41a7db872f98e98a7f6665bf0ea152dbc7b684b2
n-e-d-a <87589027+n-e-d-a@users.noreply.github.com>
2021-11-05 07:39:37 +0330

    added api dictionary

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,20 @@
+import axios from "axios";
 import {React,useState} from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
 let [keyword,setKeyword]=useState(null);
 
-    function search(event){
-        event.preventDefault();
-        alert(`Searching for the ${keyword} definition`);
-    }
+function handleResponse(response){
+    console.log(response.data[0]);
+}   
+
+function search(event){
+  event.preventDefault();
+  //documentation: https://dictionaryapi.dev/
+  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+  axios.get(apiUrl).then(handleResponse);
+}
 function handleKeywordChange(event){
     setKeyword(event.target.value);
 }
