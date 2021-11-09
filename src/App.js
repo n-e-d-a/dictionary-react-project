85ff60ae393dbdee6f3b1d8727da65c9ae408059
n-e-d-a <87589027+n-e-d-a@users.noreply.github.com>
2021-11-09 09:06:36 +0330

    hosted on Netlify added

--- src/App.js
+++ src/App.js
@@ -10,13 +10,39 @@ export default function App() {
           <img src={logo} className="App-logo img-fluid" alt="logo" />
         </header>
         <main>
-          <Dictionary defaultKeyWord="sunrise"/>
+          <Dictionary defaultKeyWord="sunrise" />
         </main>
         <footer className="App-footer">
-          Coded by{" "}
+          {/* Coded by{" "}
           <a href="https://xenodochial-poitras-5501a5.netlify.app">
             {" "}
             Neda Zare
+          </a>
+        </footer>
+        <footer> */}
+          This project was coded by{" "}
+          <a
+            href="https://xenodochial-poitras-5501a5.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Neda Zare
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/n-e-d-a/dictionary-react-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open-Sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://dazzling-torvalds-fc5d49.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Hosted on Netlify
           </a>
         </footer>
       </div>
